6a158641764794e82a8e744ab847841c14546618
Vishesta Jain <vishesta76@gmail.com>
2019-09-10 17:07:56 +0530

    initial

--- changepassword.php
+++ changepassword.php
@@ -1,29 +1,36 @@
-
-
-    <div class="row">
-        <div class="well col-md-5 center login-box" id="well">
-            <div class="alert alert-info">
-                Change Password for your Account.
-            </div>
-            <form class="form-horizontal" action="password.php" method="post" autocomplete="off">
-                <fieldset>
-                    <div class="input-group input-group-lg">
-                        <span class="input-group-addon"><i class="glyphicon glyphicon-user red"></i></span>
-                        <input type="text" class="form-control" placeholder="Name" name="name" required>
-                    </div>
-                    <div class="clearfix"><br></div>
-
-                    <div class="input-group input-group-lg">
-                        <span class="input-group-addon"><i class="glyphicon glyphicon-lock red"></i></span>
-                        <input type="password" class="form-control" placeholder="New Password" name="password1" required>
-                    </div>
-                    <div class="clearfix"><br></div>
-
-                    <div class="input-group input-group-lg">
-                        <span class="input-group-addon"><i class="glyphicon glyphicon-lock red"></i></span>
-                        <input type="password" class="form-control" placeholder="Confirm Password" name="password2" required>
-                    </div>
-                    <div class="clearfix"></div>
+<!DOCTYPE html>
+<html>
+ <head>
+  <title>Image Gallery </title>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
+</head>
+ <br />
+  
+  <br />
+  <body> 
+ <div class="container form_style">
+   <div class="panel panel-default" >
+    <div class="panel-heading">
+     <h3 class="panel-title">Change your Password</h3>
+    </div>
+    <div class="panel-body">
+     <form class="form-horizontal" action="password.php" method="post" autocomplete="off">
+      <div class="form-group">
+       <label>Enter Your Username:</label>
+       <input type="text" class="form-control" placeholder="Name" name="name" required>
+      </div>
+      <div class="form-group">
+       <label>Enter Your Password</label>
+       <input type="password" class="form-control" placeholder="New Password" name="password1" required>
+      </div>
+       <div class="form-group">
+       <label>Re-Enter Your Password</label>
+        <input type="password" class="form-control" placeholder="Confirm Password" name="password2" required>
+      </div>
+      
+      <div class="form-group" align="center">
+      
+            
                     <?php
                         if (isset($_SESSION['reset_error'])) { ?>
                             <div class="alert alert-warning">
@@ -39,7 +46,9 @@
                     <p class="center col-md-5">
                         <button name="reset" class="btn btn-primary">Reset Password</button>
                     </p>
-                </fieldset>
+                
             </form>
         </div>
     </div>
+</body>
+</html>
